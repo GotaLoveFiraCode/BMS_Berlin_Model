c2aacb49c564a93d39271080e92d1e66ae98e18c
Laurens T. Rosinski <techwebsavvy@pm.me>
2023-05-30 17:12:09 +0200

    Added some polishing

--- berlin.rb
+++ berlin.rb
@@ -3,21 +3,26 @@
 class Berlin
   include PiPiper
 
+  # create pins, assign values
   def initialize
-    @gpio_out = Pin.new(:pin => 2, :direction => :out)
-    @gpio_in = Pin.new(:pin => 4, :direction => :in)
-    @gpio_in_off = Pin.new(:pin => 17, :direction => :in)
-  end
+    @gpio_out = Pin.new(:pin => 2, :direction => :out) # output
+    @gpio_in = Pin.new(:pin => 4, :direction => :in) # read source one
+    @gpio_in_off = Pin.new(:pin => 17, :direction => :in) # read source two, might not be neccesary
+  end # init
 
-  def gui_test
-    puts "=> GUI test successful!"
-  end
+  # just make sure this is working
+  def test
+    puts "=> test successful!"
+  end # test
 
+  # Turn stuff off, if neccesary
   def shutdown
     puts "=> powering off"
-    puts "==> WARNING: this funktion has not yet been created!"
-  end
+    puts "==> WARNING: this funktion has not yet been completed!"
+    @gpio_out.off
+  end # shutdown
 
+  # Turn `berlin' on/off
   def lights(set)
 
     if set == true
@@ -25,27 +30,29 @@ class Berlin
     else if set == false
       @gpio_out.off
     else
-      puts "=> ERROR: wrong input for `river_lights'"
-    end
+      puts "=> ERROR: wrong input for `lights(mode)'"
+    end # if
 
-  end
+  end # lights
 
   def button
 
     after @gpio_in, :goes => :high do
       puts "=> Button activated, set to high"
       lights(true)
-    end
+    end # do, after
 
-    # after @gpio_in, :goes => :low do
-    #   puts "=> Button deactivated, set to low"
-    #   lights(false)
-    # end
+    after @gpio_in, :goes => :low do
+      puts "=> Button deactivated, set to low"
+      lights(false)
+    end # do, after
 
     after @gpio_in_off, :goes => :high do
       puts "=> Button deactivated, set to low"
       lights(false)
-    end
-  end
-end
+    end # do, after
+
+  end # button
+
+end # Berlin
 
